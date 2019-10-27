e6306472ce8a5d152f1a5e0bc46577e51b58c41a
Gideon Tong <gideon@gideontong.com>
2019-10-27 01:04:54 -0700

    Fix variable mismatch

--- api/src/main.c
+++ api/src/main.c
@@ -42,7 +42,7 @@ void setup() {
     // Connect to Wi-Fi network with SSID and password
     Serial.print("Setting AP (Access Point)…");
     // Remove the password parameter, if you want the AP (Access Point) to be open
-    WiFi.softAP(ssid, password);
+    WiFi.softAP(ssid, psk);
 
     IPAddress IP = WiFi.softAPIP();
     Serial.print("AP IP address: ");
