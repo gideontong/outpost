4adee6e786098f691bbf3b194c2147db5a1aff3d
Brian Lam <brilam8@gmail.com>
2019-10-26 22:15:00 -0700

    text interface set-up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { Fragment } from "react";
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import InputGroup  from 'react-bootstrap/InputGroup';
+import InputGroupAppend  from 'react-bootstrap/InputGroup';
+import Form from 'react-bootstrap/Form'
+import ListGroup from 'react-bootstrap/ListGroup'
+
+function App() {  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="row">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"/>
+      <div className="center">
+          <Form id = "textMessagingArea">
+              <ListGroup >
+                <ListGroup.Item>Brian: Spaghetti</ListGroup.Item>
+                <ListGroup.Item>Max: Putin Putout</ListGroup.Item>
+                <ListGroup.Item>Shan: I missed my math midterm</ListGroup.Item>
+                <ListGroup.Item>Vincent: welcome to the farmer's market</ListGroup.Item>
+                <ListGroup.Item>Gideon: lol c++ embedded libraries</ListGroup.Item>
+                
+              </ListGroup>
+              
+                <div class="form-inline" id = "textBar">
+                    <textarea class="form-control"  id="sendTextArea" placeholder="Aa" rows = '1' />
+                    <Button variant = "link" id = "button"><img src={"/send-arrow.png"} id = "sendArrowImage"></img> </Button> 
+                </div>
+              
+          </Form>
+      </div>
+  </div>
   );
 }
 
