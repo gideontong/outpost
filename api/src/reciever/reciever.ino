2eea632ed5204591d080a59ae8f02bc6d8ae1e56
Gideon Tong <gideon@gideontong.com>
2019-10-27 04:03:15 -0700

    Added some debug statements

--- api/src/reciever/reciever.ino
+++ api/src/reciever/reciever.ino
@@ -26,7 +26,7 @@ void progressDraw() {
     Heltec.display -> clear();
     Heltec.display -> drawProgressBar(0, 32, 120, 10, progress);
     Heltec.display -> setTextAlignment(TEXT_ALIGN_CENTER);
-    Heltec.display -> drawString(64, 15, String(progress) + "%");
+    Heltec.display -> drawString(64, 15, "Setup: " + String(progress) + "%");
     Heltec.display -> display();
 }
 
@@ -40,9 +40,17 @@ void setup() {
     // Setting up the WiFi
     Serial.print("Setting up AP...");
     WiFi.softAP(ssid, psk);
-    progress = 50;
+    Serial.println(" Success!");
+    progress = 33;
     progressDraw();
     
+    // Make the IP address available
+    Serial.print("IP address is: ");
+    IPAddress IP = WiFi.softAPIP();
+    Serial.println(IP);
+    progress = 66;
+    progressDraw();
+
     // Start the web server
     server.begin();
     progress = 100;
