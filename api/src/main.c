dc1a17d45aab30cd9888fd206b188e599222e0f6
Gideon Tong <gideon@gideontong.com>
2019-10-27 01:03:30 -0700

    Replace setup script

--- api/src/main.c
+++ api/src/main.c
@@ -32,6 +32,23 @@ const int output27 = 27;
 
 void setup() {
     Serial.begin(115200);
+    // Initialize the output variables as outputs
+    pinMode(output26, OUTPUT);
+    pinMode(output27, OUTPUT);
+    // Set outputs to LOW
+    digitalWrite(output26, LOW);
+    digitalWrite(output27, LOW);
+
+    // Connect to Wi-Fi network with SSID and password
+    Serial.print("Setting AP (Access Point)…");
+    // Remove the password parameter, if you want the AP (Access Point) to be open
+    WiFi.softAP(ssid, password);
+
+    IPAddress IP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(IP);
+
+    server.begin();
 }
 
 void loop() {
