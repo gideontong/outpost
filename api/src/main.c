f9db93c77c05ea7e6be5a81af85e02d39154ffa0
Gideon Tong <gideon@gideontong.com>
2019-10-27 01:03:09 -0700

    Add system variables and libraries

--- api/src/main.c
+++ api/src/main.c
@@ -1,5 +1,34 @@
-#include <stdio.h>
-#include <espidf.h>
+/*
+ *
+ * Outpost - Setup Code
+ * ====================
+ * 
+ * Intended for the ESP32 with HelTec V2 LoRa and OLED
+ * 
+ * Parts of this code were adapted from:
+ * Random Nerd Tutorials
+ * 
+ */
+
+#include "Arduino.h"
+#include "WiFi.h"
+#include "heltec.h"
+
+const char* ssid = "OutpostDev";
+const char* psk  = "test1234";
+
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+// Auxiliar variables to store the current output state
+String output26State = "off";
+String output27State = "off";
+
+// Assign output variables to GPIO pins
+const int output26 = 26;
+const int output27 = 27;
 
 void setup() {
     Serial.begin(115200);
