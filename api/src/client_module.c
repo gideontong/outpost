0c9ec0de68e642b2ce4041543eeea89ed6e7d0d1
Gideon Tong <gideon@gideontong.com>
2019-10-27 04:28:19 -0700

    Allowed reciever to track number of clients connected

--- api/src/client_module.c
+++ api/src/client_module.c
@@ -14,6 +14,7 @@
 
 // Definitions
 #define     BAND        915E6           // Set to 915MHz IHS
+#define     MAX_CLIENTS 4               // Maximum number of WiFi clients
 const char* ssid        = "OutpostDev"; // WiFi name
 const char* psk         = "test1234";   // WiFi password
 int         progress    = 0;            // Progress bar indicator
@@ -27,6 +28,16 @@ void progressDraw() {
     Heltec.display -> drawProgressBar(0, 32, 120, 10, progress);
     Heltec.display -> setTextAlignment(TEXT_ALIGN_CENTER);
     Heltec.display -> drawString(64, 15, "Booting Outpost: " + String(progress) + "%");
+    Heltec.display -> display();
+}
+
+// Draws the status screen
+void viewStatus() {
+    int clients = WiFi.softAPgetStationNum();
+    Heltec.display -> clear();
+    Heltec.display -> drawProgressBar(0, 32, 120, 10, (0.0 + clients) / MAX_CLIENTS * 100);
+    Heltec.display -> setTextAlignment(TEXT_ALIGN_CENTER);
+    Heltec.display -> drawString(64, 15, "Connected: " + String(clients) + "/" + String(MAX_CLIENTS));
     Heltec.display -> display();
 }
 
@@ -59,6 +70,7 @@ void setup() {
 
 void loop() {
     WiFiClient client = server.available();
+    viewStatus();
 
     if(client) {
         Serial.println("New device connected!");
@@ -69,11 +81,15 @@ void loop() {
                 Serial.write(c);
                 header += c;
                 if(c == '\n') {
-                    if(currentLine.length == 0) {
+                    if(currentLine.length() == 0) {
                         client.println("HTTP/1.1 200 OK");
                         client.println("Content-type:text/html");
                         client.println("Connection: close");
                         client.println();
+                        client.println("<!DOCTYPE html><html>");
+                        client.println("<head><title>Outpost</title></head>");
+                        client.println("<body><h1>Outpost</h1></body>");
+                        client.println("</html>");
                         client.println();
                         break;
                     } else {
