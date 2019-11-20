f06f3d56759d4aaece9dde8e6d81332897c4ba65
Brian Lam <brilam8@gmail.com>
2019-11-20 15:07:18 -0800

    mostly completed about page

--- frontend/src/components/aboutPage.js
+++ frontend/src/components/aboutPage.js
@@ -25,7 +25,9 @@ import Image from 'react-bootstrap/Image'
 }*/
 function aboutPage(props){
   return (
-    <div >
+
+    <div>
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous"/>
 
       <h1 class = "text-center">About Outpost: </h1>
       <Jumbotron fluid>
@@ -38,6 +40,18 @@ function aboutPage(props){
                 <h3 class = "pt-2">Gideon Tong</h3>
                 <p class = "pt-2">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
               </div>
+              <div class = "text-center">
+                  <a href="https://github.com/gideontong/">
+                    <button type="button" class="btn btn-primary bmd-btn-fab">
+                      <span><img className = "icons" src = "/github_icon.png"></img></span>
+                    </button>
+                  </a>
+                  <a href="https://www.linkedin.com/in/gideontong/">
+                    <button type="button" class="btn btn-primary bmd-btn-fab">
+                      <span><img className = "icons2" src = "/linkedin_icon.png"></img></span>
+                    </button>
+                  </a>
+                </div>
             </Col>
             <Col>
               <div class = "text-center">
@@ -45,16 +59,17 @@ function aboutPage(props){
                 <h3 class = "pt-2">Brian Lam</h3>
                 <p class = "pt-2">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
               </div>
-                <div class = "text-left">
-                  <Image className = "icons" class = "inline"src = "/github_icon.png" rounded/>
-                  <p className = "linkText">
-                    <a href="https://github.com/Brooseidon">@Brooseidon</a>
-                  </p>
-                  
-                  <Image className = "icons" src = "/linkedin_icon.png" rounded/>
-                  <p className = "linkText">
-                    <a href="https://www.linkedin.com/in/brian-lam-2955b5165/">@Brian Lam</a>
-                  </p>
+                <div class = "text-center">
+                  <a href="https://github.com/Brooseidon/">
+                    <button type="button" class="btn btn-primary bmd-btn-fab">
+                      <span><img className = "icons" src = "/github_icon.png"></img></span>
+                    </button>
+                  </a>
+                  <a href="https://www.linkedin.com/in/brian-lam-2955b5165/">
+                    <button type="button" class="btn btn-primary bmd-btn-fab">
+                      <span><img className = "icons2" src = "/linkedin_icon.png"></img></span>
+                    </button>
+                  </a>
                 </div>
             </Col>
           </Row>
