a28c7867dcc371f145e9ae51555f5e1d14a5bdf8
Gideon Tong <gideon@gideontong.com>
2020-01-05 13:15:02 -0800

    Refactored remaining frontend components

--- frontend/src/components/TextSend.js
+++ frontend/src/components/TextSend.js
@@ -4,43 +4,44 @@ import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
-class TextSend extends React.Component{
-  
+class TextSend extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       value: ''
     }
   }
+
   buttonPress(e) {
-    this.setState({value: e.target.value})
+    this.setState({ value: e.target.value })
     if (this.state.value !== undefined && this.state.value !== '') {
-      const obj = {message: this.state.value,
-      name: "Brian"};
+      const obj = {
+        message: this.state.value,
+        name: "Brian"
+      };
       console.log('POST SENT');
       axios.post('http://localhost:9000/api/messages/addmessage', obj)
         .then(res => console.log(res.data));
-        
     }
+
     this.myinput.value = "";
     console.log(this.state.value);
     window.location.reload();
   }
+
   handleChange = (e) => {
-    this.setState({value: e.target.value});
-    
-    console.log("text box changed"+this.state.value)
-    
-  }
-  render() {
-      return (
-        <div className="form-inline" id = "textBar">
-            <textarea className="form-control"  onChange={(e) => {this.handleChange(e)}} id="sendTextArea" placeholder="Type here..." rows = '1' ref={(input)=> this.myinput = input}/>
-            <Button variant = "link" onClick={this.buttonPress.bind(this)} id = "button" ><img src={"/send-arrow.png"} alt = "sendImage" id = "sendArrowImage"></img> </Button> 
-        </div>
-      )
+    this.setState({ value: e.target.value });
+    console.log("text box changed" + this.state.value)
   }
 
+  render() {
+    return (
+      <div className="form-inline" id="textBar">
+        <textarea className="form-control" onChange={(e) => { this.handleChange(e) }} id="sendTextArea" placeholder="Type here..." rows='1' ref={(input) => this.myinput = input} />
+        <Button variant="link" onClick={this.buttonPress.bind(this)} id="button" ><img src={"/send-arrow.png"} alt="sendImage" id="sendArrowImage"></img> </Button>
+      </div>
+    )
+  }
 }
 
 export default TextSend;
--- frontend/src/components/textInterface.js
+++ frontend/src/components/textInterface.js
@@ -4,20 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import MessagesBox from './MessagesBox'
 import TextSend from './TextSend'
 import Form from 'react-bootstrap/Form'
-
-class textInterface extends React.Component{
+class textInterface extends React.Component {
   render() {
     return (
-      <div className="row"> 
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"/>
-          <div className="center">
-              <Form id = "textMessagingArea" method = "post" action = "/addmessage">
-                  <MessagesBox/>
-                  
-                    <TextSend/>
-                  
-              </Form>
-          </div>
+      <div className="row">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
+        <div className="center">
+          <Form id="textMessagingArea" method="post" action="/addmessage">
+            <MessagesBox />
+            <TextSend />
+          </Form>
+        </div>
       </div>
     )
   }
